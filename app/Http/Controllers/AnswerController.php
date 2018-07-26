09bfc9629bd9d440078c2b4a332d6d7760b85f41
peter <kelinipeter@gmail.com>
2018-07-26 18:55:31 -0400

    add the views to display the seed data for the user profile, user home page to display questions, the question page that displays the answers to question in the sideba

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Answer;
 
 class AnswerController extends Controller
 {
@@ -11,6 +12,11 @@ class AnswerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         //
