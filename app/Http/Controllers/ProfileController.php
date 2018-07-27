6d48574217eabaf1da4aed27aa71e9ee7382479e
peter <kelinipeter@gmail.com>
2018-07-26 21:29:13 -0400

    added flash folder

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Profile;
+use App\User;
+
 
 
 
@@ -26,7 +29,9 @@ class ProfileController extends Controller
      */
     public function create()
     {
-        //
+        $profile = new Profile();
+        $edit = FALSE;
+        return view('profileForm', ['profile' => $profile,'edit' => $edit  ]);
     }
 
     /**
@@ -37,7 +42,20 @@ class ProfileController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $input = $request->validate([
+            'fname' => 'required',
+            'lname' => 'required',
+            'body' => 'required',
+        ], [
+            'fname.required' => ' First is required',
+            'lname.required' => ' Last is required',
+            'body.required' => ' Body is required',
+        ]);
+        $input = request()->all();
+        $profile = new Profile($input);
+        $profile->user()->associate(Auth::user());
+        $profile->save();
+        return redirect()->route('home')->with('message', 'Profile Created');
     }
 
     /**
@@ -60,11 +78,13 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($user, $profile)
     {
-        //
+        $user = User::find($user);
+        $profile = $user->profile;
+        $edit = TRUE;
+        return view('profileForm', ['profile' => $profile, 'edit' => $edit ]);
     }
-
     /**
      * Update the specified resource in storage.
      *
@@ -72,19 +92,20 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request,  $user, $profile)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $input = $request->validate([
+            'fname' => 'required',
+            'lname' => 'required',
+        ], [
+            'fname.required' => ' First is required',
+            'lname.required' => ' Last is required',
+        ]);
+        $profile = Profile::find($profile);
+        $profile->fname = $request->lname;
+        $profile->lname = $request->lname;
+        $profile->body = $request->body;
+        $profile->save();
+        return redirect()->route('home')->with('message', 'Updated Profile');
     }
 }
